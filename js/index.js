97a2d3102e43c12a1d7e9e996db4af63a96d3f85
Käkäsh <marco.zillgen23@gmail.com>
2023-02-28 18:07:50 +0100

    gute push nachricht

--- js/index.js
+++ js/index.js
@@ -1,76 +1,114 @@
-// Population(age) + 2000 * Fertility(age) / Population(age) - Mortility(age)
-
-// get data from json
 const fs = require("fs");
+
 const data_pop = fs.readFileSync("./data/pop.json");
 const data_fert = fs.readFileSync("./data/fert.json");
 const data_mort = fs.readFileSync("./data/mort.json");
-
-// parse data to json object
 const pop = JSON.parse(data_pop);
 const fert = JSON.parse(data_fert);
 const mort = JSON.parse(data_mort);
-
 pop.shift();
 fert.shift();
 mort.shift();
 
-/**
- * region / country
- * type (is it country / region /...)
- * Year
- * Population (in 1000)
- * 0age
- * 1age
- * 2age...
- * 100+age
- */
+const data = initPopData("2021", ["WORLD"])
+const data1 = simulateYear(data)
 
-// start timer
-console.time("timer");
-
-// for (let i = 0; i < pop.length; i++) {
-//     console.log(pop[i]["__EMPTY"]);
-//     console.log(pop[i]["__EMPTY_1"]);
-//     console.log(pop[i]["__EMPTY_2"]);
-//     console.log(" ")
-// }
-
-// stop timer
-
-const data = initData()
-// write data as json in data/
-
-console.timeEnd("timer");
-
-/**
- * Diese Funktion konvertiert die Daten aus den JSON-Dateien in das gewünschte Format.
- * Sie durchläuft jeden Eintrag in der 'pop'-Datei und erstellt ein Objekt für jeden Eintrag.
- * 
- * Hinweis: In diesem Beispiel wird angenommen, dass alle Einträge in der 'pop'-Datei
- * das Jahr "2021" haben. Wenn dies nicht der Fall ist, wird das Objekt nicht erstellt.
- * 
- * Rückgabe: Ein Array von Objekten im gewünschten Format. */
-function initData() {
+function initPopData(year = "2021", countries = undefined) {
     let data = [];
 
+    // population
     for (let i = 0; i < pop.length; i++) {
-        if (pop[i]["__EMPTY_2"] != "2021") return
+        if (pop[i]["__EMPTY_2"] == year && (countries === undefined || countries.includes(pop[i]["__EMPTY"]))) {
+            let obj = {
+                type: pop[i]["__EMPTY_1"],
+                year: pop[i]["__EMPTY_2"],
+                pop: [],
+                // mort: [],
+                // fert: []
+            }
 
-        let obj = {
-            region: pop[i]["__EMPTY"],
-            type: pop[i]["__EMPTY_1"],
-            year: pop[i]["__EMPTY_2"],
-            totalPopulation: pop[i]["__EMPTY_103"],
-            age: []
+            for (let j = 3; j < 103; j++) {
+                obj.pop.push(pop[i]["__EMPTY_" + j]);
+            }
+            data[pop[i]["__EMPTY"]] = obj
         }
-
-        for (let j = 4; j < pop[i].length; j++) {
-            obj.age.push(Object.entries(pop[i])[j]);
-        }
-        data.push(obj);
     }
 
+    // // mortility
+    // for (let i = 0; i < mort.length; i++) {
+    //     if (mort[i]["__EMPTY_10"] == year && (countries === undefined || countries.includes(mort[i]["__EMPTY_2"]))) {
+    //         for (let j = 11; j < 111; j++) {
+    //             data[mort[i]["__EMPTY_2"]].mort.push(mort[i]["__EMPTY_" + j]);
+    //         }
+    //     }
+    // }
+
+    // // fertility
+    // for (let i = 0; i < fert.length; i++) {
+    //     if (fert[i]["__EMPTY_10"] == year && (countries === undefined || countries.includes(fert[i]["__EMPTY_2"]))) {
+    //         for (let j = 11; j < 45; j++) {
+    //             data[fert[i]["__EMPTY_2"]].fert.push(fert[i]["__EMPTY_" + j]);
+    //         }
+    //     }
+    // }
+
     return data;
+}
+
+function simulateYear(countries, death_rate = 0.075, birth_rate = 0.06605) {
+    console.log(countries)
+    // give type of countries
+    console.log(typeof countries)
+
+    for(let i = 0; i < countries.length;i++){
+        console.log(2);
+        const country = countries[i]
+        
+        for (let i = country.pop.length - 2; i > 0; i--) {
+            country.pop[i + 1] = country.pop[i] * (1 - death_rate)
+        }
+
+        // 0 age
+        country.pop[0] = getChildbearingWomen(pop, fert) * birth_rate
+    }
+
+    // countries.forEach((country) => {
+    //     console.log(country);
+
+    //     for (let i = country.pop.length - 2; i > 0; i--) {
+    //         country.pop[i + 1] = country.pop[i] * (1 - death_rate)
+    //     }
+
+    //     // 0 age
+    //     country.pop[0] = getChildbearingWomen(pop, fert) * birth_rate
+
+    // })
+
+    return countries;
+}
+
+function getChildbearingWomen(pop) {
+    let all = 0;
+
+    // women between 15 and 49
+
+    pop.foreach((age, i) => {
+        if (age >= 15 && age <= 49)
+            all += age / 2
+    })
+
+    return all;
+}
+
+function getTotalPopulation(countries) {
+    let pop = 0;
+
+    countries.foreach((country) => {
+        country.pop.foreach((age) => {
+            pop += age
+        })
+    })
+
+    return pop
 }
 
