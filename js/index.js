73a3eba49a60c0d722de499a6910de91df50f5b5
Käkäsh <marco.zillgen23@gmail.com>
2023-03-01 11:27:01 +0100

    push

--- js/index.js
+++ js/index.js
@@ -13,9 +13,7 @@ mort.shift();
 const data = initPopData("2021", ["WORLD"])
 console.log(data)
 const data1 = simulateYear(data)
-data[0].pop.forEach((age,i) => {
-    console.log(i + ":" + age)
-})
+console.log(data)
 
 function initPopData(year = "2021", countries = undefined) {
     let data = [];
@@ -28,8 +26,8 @@ function initPopData(year = "2021", countries = undefined) {
                 type: pop[i]["__EMPTY_1"],
                 year: pop[i]["__EMPTY_2"],
                 pop: [],
-                // mort: [],
-                // fert: []
+                mort: [],
+                fert: []
             }
 
             obj.pop.push(pop[i]["Total population by single age, both sexes combined (thousands)"])
@@ -40,33 +38,17 @@ function initPopData(year = "2021", countries = undefined) {
         }
     }
 
-    // // mortility
-    // for (let i = 0; i < mort.length; i++) {
-    //     if (mort[i]["__EMPTY_10"] == year && (countries === undefined || countries.includes(mort[i]["__EMPTY_2"]))) {
-    //         for (let j = 11; j < 111; j++) {
-    //             data[mort[i]["__EMPTY_2"]].mort.push(mort[i]["__EMPTY_" + j]);
-    //         }
-    //     }
-    // }
 
-    // // fertility
-    // for (let i = 0; i < fert.length; i++) {
-    //     if (fert[i]["__EMPTY_10"] == year && (countries === undefined || countries.includes(fert[i]["__EMPTY_2"]))) {
-    //         for (let j = 11; j < 45; j++) {
-    //             data[fert[i]["__EMPTY_2"]].fert.push(fert[i]["__EMPTY_" + j]);
-    //         }
-    //     }
-    // }
 
     return data;
 }
 
-function simulateYear(countries, death_rate = 0.0075, birth_rate = 0.06605) {
+function simulateYear(countries, birth_rate = 0.06605) {
     countries.forEach((country) => {
         country.year++
 
         for (let i = country.pop.length - 2; i >= 0; i--) {
-            country.pop[i + 1] = country.pop[i] * (1 - death_rate)
+            country.pop[i + 1] = country.pop[i] * (1 - 0.0075)
         }
 
         // 0 age
@@ -79,13 +61,13 @@ function simulateYear(countries, death_rate = 0.0075, birth_rate = 0.06605) {
 function getChildbearingWomen(population) {
     let all = 0;
 
-    // women between 15 and 49
-
     population.forEach((age, i) => {
         if (i >= 15 && i <= 49) {
             all += age / 2
         }
     })
+
+    console.log(all)
 
     return all;
 }
@@ -101,4 +83,3 @@ function getTotalPopulation(countries) {
 
     return pop
 }
-
