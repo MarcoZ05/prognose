02cfa79b9e4ab49279eedb53978fd8db4b781604
Käkäsh <marco.zillgen23@gmail.com>
2023-03-02 08:58:16 +0100

    :(

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 // start timer
-console.timer("timer")
+console.time("timer");
 
 const data_pop = fs.readFileSync("./data/pop.json");
 const data_fert = fs.readFileSync("./data/fert.json");
@@ -13,10 +13,27 @@ pop.shift();
 fert.shift();
 mort.shift();
 
-const data = initPopData("2021", ["Germany"])
-console.log(data)
-simulateYear(data)
-console.log(data)
+let startYear = 2021
+const endYear = 2022
+const countries = ["WORLD"]
+
+const popArr = []
+
+const data = initPopData(
+    startYear.toString(), countries
+)
+
+for (let i = startYear; i <= endYear; i++) {
+    const newData = simulateYear(JSON.parse(JSON.stringify(data)))
+    popArr.push(newData)
+    if (i == endYear)
+        fs.writeFileSync("./data/data.js", "export default " + JSON.stringify(newData));
+}
+
+fs.writeFileSync("./data/pop.js", "export default " + JSON.stringify(data1));
+
+// end timer
+console.timeEnd("timer");
 
 function initPopData(year = "2021", countries = undefined) {
     let data = [];
